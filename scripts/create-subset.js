ed24d9833e061f9249cfad5808cc5a37f2d45abd
Tatsuya Kinno <lawson.and.7.11@gmail.com>
2020-03-19 03:28:55 +0900

    モルダウ

--- scripts/create-subset.js
+++ scripts/create-subset.js
@@ -9,7 +9,7 @@ const html = fs.readFileSync(SOURCE_FILE, 'utf-8')
 const dom = new JSDOM(html)
 const { body } = dom.window.document
 
-const texts = [...Array.from(body.querySelectorAll('h1,li')).map(el => el.textContent).join('')]
+const texts = [...Array.from(body.querySelectorAll('h1,li,p,button')).map(el => el.textContent).join('')]
 const subset = texts.filter((str, i, self) => self.indexOf(str) === i).join('')
 
 fs.writeFileSync(DEST_FILE, subset)

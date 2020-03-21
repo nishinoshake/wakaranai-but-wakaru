5b947715a4cd8e8368e3214918ff7b4cf72cb23b
Tatsuya Kinno <lawson.and.7.11@gmail.com>
2020-03-22 03:21:29 +0900

    全画面

--- scripts/create-subset.js
+++ scripts/create-subset.js
@@ -3,15 +3,13 @@ const path = require('path')
 const { JSDOM } = require('jsdom')
 
 const SOURCE_FILE = path.resolve(__dirname, '../src/index.html')
-const KANA_FILE = path.resolve(__dirname, './subset-kana.txt')
-const KANJI_FILE = path.resolve(__dirname, './subset-kanji.txt')
+const DEST_FILE = path.resolve(__dirname, './subset.txt')
 
-const kana = [...fs.readFileSync(KANA_FILE, 'utf-8')]
 const html = fs.readFileSync(SOURCE_FILE, 'utf-8')
 const dom = new JSDOM(html)
 const { body } = dom.window.document
 
 const texts = [...Array.from(body.querySelectorAll('h1,li,p,button')).map(el => el.textContent).join('')]
-const subset = texts.filter(str => !kana.includes(str)).filter((str, i, self) => self.indexOf(str) === i).join('')
+const subset = texts.filter((str, i, self) => self.indexOf(str) === i).join('')
 
-fs.writeFileSync(KANJI_FILE, subset)
+fs.writeFileSync(DEST_FILE, subset)
